df64471d634dbca371508257ceb0b3c775459acd
Ray Ortiz <oray@bgsu.edu>
2022-05-16 20:56:33 -0400

    Import App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"; //Import and use both functions for rendering
 import { ethers } from "ethers"; //Interacting with our contract
 import Manager from "./artifacts/contracts/Manager.sol/Manager.json"; //Contains ABI, which will instantiate and interact w/ our contract
+import "./App.css";
 
 function App() {
   const [name, setName] = useState("");
